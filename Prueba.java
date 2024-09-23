85afe5a7fcf22481b7415218f42b771a3d570ede
daniel <danielmateos684@gmail.com>
2024-09-23 12:50:25 +0200

    Mi red social favorita

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba {
 	public static void main(String args[]){
 
 		System.out.println("Baloncesto");
-		System.out.println("Suiza")
+		System.out.println("Suiza");
+		System.out.println("Instagram");
 	}
 }
